908c35ec9ace979111a93c942b0d7e9a3eb5cfd6
toiletsplurge <MrTakeTooMuch@gmail.com>
2023-03-27 15:09:40 -0400

    More styling & responsive updates
    Fixed(?) navbar

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const navbarLinks = document.getElementById("navbar-links");
 const toggleButton = document.querySelector(".toggle-button");
+const navbarLinks = document.querySelector(".navbar-links");
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
+  toggleButton.classList.toggle("active");
+});
+
+navbarLinks.addEventListener("click", () => {
+  navbarLinks.classList.remove("active");
+  toggleButton.classList.remove("active");
 });
